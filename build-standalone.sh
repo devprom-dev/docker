de271ab023ad56fe44939f9eb98df55d164ce480
devprom-dev <devprom-dev@users.noreply.github.com>
2024-11-21 11:34:28 +0300

    Update build-standalone.sh

--- build-standalone.sh
+++ build-standalone.sh
@@ -10,6 +10,10 @@ ENV MYSQL_PASSWORD=
 ENV DEBIAN_FRONTEND noninteractive
 
 #
+VOLUME /var/www/devprom
+VOLUME /var/lib/mysql
+
+#
 RUN apt-get -y update && apt-get -y install apache2 default-mysql-server default-mysql-client \
   php7.4 php7.4-mysql libapache2-mod-php7.4 php7.4-gd php7.4-common php7.4-bcmath \
   php7.4-mysqli php7.4-curl php7.4-imap php7.4-ldap php7.4-xml php7.4-mbstring php7.4-zip php7.4-imagick \
@@ -42,14 +46,6 @@ COPY smtp/smtpd.conf /etc/postfix/sasl/smtpd.conf
 RUN echo "* * * * * www-data /usr/bin/php /var/www/devprom/htdocs/core/processjobs.php >/dev/null 2>&1" >>  /etc/crontab
 RUN echo "" >>  /etc/crontab
 RUN mkdir -p /var/www/devprom
-
-#
-VOLUME /var/www/devprom
-
-#
-RUN mkdir /var/www/devprom/backup  && mkdir /var/www/devprom/update && \
-  mkdir /var/www/devprom/files && mkdir /var/www/devprom/logs
-RUN chown -R www-data:www-data /var/www/devprom && chmod -R 755 /var/www/devprom
 
 #
 RUN rm /etc/apache2/sites-available/* && rm /etc/apache2/sites-enabled/*
