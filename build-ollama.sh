dd05a2d32e620c97dcdb1e1dbea25825896a76ee
devprom-dev <devprom-dev@users.noreply.github.com>
2026-02-25 20:49:47 +0300

    Update build-ollama.sh

--- build-ollama.sh
+++ build-ollama.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+cat << _EOF_ > Dockerfile
+FROM ollama/ollama:latest
+
+COPY ollama/entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
+_EOF_
+
 docker pull ollama/ollama:latest
 docker build -f Dockerfile.ollama -t devprom/ollama:latest .
 docker push devprom/ollama:latest
